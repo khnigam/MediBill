2f57042b6828213bdfed123c8511d0a715f294c7
nigam <nigamk@advanatageclub.co>
2026-04-21 14:50:07 +0530

    apis

--- src/main/java/com/ujjwalMedical/MediBillApplication.java
+++ src/main/java/com/ujjwalMedical/MediBillApplication.java
@@ -19,3 +19,24 @@ public class MediBillApplication {
 //https://codetest.advantageclub.co/appreciation_config/get_users_details?search
 //https://codetest.advantageclub.co/appreciation_config/options
 //c64aae47a5457e330eeed0363ba976f58781b7f1
+//{
+//    "users": [
+//        {
+//            "id": 81308,
+//            "full_name": "Demo Account",
+//            "email": "demo@advantageclub.in",
+//            "employee_id": null
+//        },
+//        {
+//            "id": 236022,
+//            "full_name": "Demo",
+//            "email": "test@advantageclub.in",
+//            "employee_id": null
+//        },
+//        {
+//            "id": 1100139,
+//            "full_name": "Admin Testing",
+//            "email": "adminactest@yopmail.com",
+//            "employee_id": null
+//        }
+//        ]
